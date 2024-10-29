c919881883a7a4f4f225094dd5de6691ad8d28e6
ghaithkhl27 <ghaithkhalfallah27@hotmail.com>
2024-10-29 16:18:34 +0100

    test

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const dishRoutes = require("./Routes/dishRoutes");
+
 const bodyParser = require("body-parser");
 const categoryRouter = require("./Routes/categoryDishRoutes.js");
 const creatdishRouter = require("./Routes/creatDishRoute.js");
 const chiefRouter = require("./Routes/chiefRoutes.js");
 const authroutes = require("./Routes/authroutes.js");
+const detailsrouter =require("./Routes/DetailsDishRoute.js")
 
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extends: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 
 app.use("/api/users", authroutes);
-app.use("/api/dish", dishRoutes);
+app.use("/api",detailsrouter)
 app.use("/api/category", categoryRouter);
 app.use("/api/create", creatdishRouter);
 app.use("/api/chief", chiefRouter);
